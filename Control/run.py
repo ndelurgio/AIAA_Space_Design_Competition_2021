a6e8c0c9ed77d6eb376c46dfbc2d63388eaf80dd
ndelurgio <delurgion@gmail.com>
2021-12-09 19:54:07 -0600

    add quaternion rotation handling

--- Control/run.py
+++ Control/run.py
@@ -21,9 +21,9 @@ EEV_state = State(q0,w0,x0,v0)
 m = 6000
 r = 4
 l = 8
-I = np.matrix([[0.25*m*r**2 + 1/12*m*l**2,0,0],[0,0.25*m*r**2 + 1/12*m*l**2,0],[0,0,m*r**2]])
+I = np.array([[0.25*m*r**2 + 1/12*m*l**2,0,0],[0,0.25*m*r**2 + 1/12*m*l**2,0],[0,0,m*r**2]])
 mdot = 0
-Idot = np.matrix([[0,0,0],[0,0,0],[0,0,0]])
+Idot = np.array([[0,0,0],[0,0,0],[0,0,0]])
 F = np.array([0,0,0])
 M = np.array([0,0,0])
 EEV = Vehicle(m,I,mdot,Idot,F,M,EEV_state)
@@ -31,9 +31,9 @@ EEV = Vehicle(m,I,mdot,Idot,F,M,EEV_state)
 ## DEBUG: RUN SIMULATION
 t = t0
 while t < tf:
-    qdot = np.array([0,0.1,0,0])
+    qdot = np.array([0,0,0,0])
     wdot = np.array([0,0,0])
-    xdot = np.array([1,1,1])
+    xdot = np.array([0,0,0])
     vdot = np.array([0,0,0])
     EEV.state.propagate(qdot,wdot,xdot,vdot,dt)
     print(EEV.state.x)
--- Control/test.py
+++ Control/test.py
@@ -5,7 +5,32 @@ def omega(w):
                            [w[1],  -w[2],     0,  w[0]],
                            [w[2],   w[1], -w[0],    0]])
         return omega
-    
-w = np.array([1,2,3])
-o = omega(w)
-print(o)
+
+def q_matrix(q):
+        qmat = np.array([[q[0],-q[1],-q[2],-q[3]],
+                         [q[1], q[0], q[3],-q[2]],
+                         [q[2],-q[3], q[0], q[1]],
+                         [q[3], q[2],-q[1], q[0]]])
+        return qmat
+def q_mult(r,s):
+    t = q_matrix(r).dot(s)
+    return t
+def rotate(q,va):
+    qinv = np.array([q[0],-q[1],-q[2],-q[3]])
+    va = np.array([0,va[0],va[1],va[2]])
+    r = q_mult(qinv,va)
+    vb = q_mult(q_mult(qinv,va),q)
+    return np.array([vb[1],vb[2],vb[3]])
+va = np.array([1,0,0])
+q = np.array([0.707,0,0,0.707])
+vb = rotate(q,va)
+print(vb)
+#x = np.array([1,2,3,4])
+#y = q_matrix(x)
+#z = y.dot(x)
+#print(y)
+#print(x)
+#print(z)
+#w = np.array([1,2,3])
+#o = omega(w)
+#print(o)
